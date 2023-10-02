61519892aa38c0f6b75374e14c0fe3a147d45e73
sharonfahler <sharon.fahler@gmail.com>
2023-10-02 12:23:22 -0400

    Submitting 49.3 Assignment

--- 49.3_Problem-Solving-Divide & Conquer/49.3.7_prob-divide-conquer_Exercise/49.3.7_prob-divide-conquer_Working Copy/count-zeroes.js
+++ 49.3_Problem-Solving-Divide & Conquer/49.3.7_prob-divide-conquer_Exercise/49.3.7_prob-divide-conquer_Working Copy/count-zeroes.js
@@ -1,5 +1,29 @@
-function countZeroes() {
-  
+console.log("testing for count-zeroes file to come through")
+
+function countZeroes(arr) {
+  // add whatever parameters you deem necessary - good luck!
+  let firstZero = findFirst(arr)
+  console.log(firstZero + "firstZero")
+  if (firstZero === -1) return 0;
+
+  return arr.length - firstZero
 }
 
-module.exports = countZeroes
+function findFirst(arr, low = 0, high = arr.length - 1) {
+  console.log('testing')
+  if (high >= low) {
+    console.log(high + low)
+    let mid = low + Math.floor((high - low) / 2)
+    console.log(mid)
+    if ((mid === 0 || arr[mid - 1] === 1) && arr[mid] === 0) {
+      console.log(high + "high" + low + "low")
+      return mid;
+
+    } else if (arr[mid] === 1) {
+      return findFirst(arr, mid + 1, high)
+    }
+    return findFirst(arr, low, mid - 1)
+  }
+  console.log("negative 1")
+  return -1;
+}
--- 49.3_Problem-Solving-Divide & Conquer/49.3.7_prob-divide-conquer_Exercise/49.3.7_prob-divide-conquer_Working Copy/find-floor.js
+++ 49.3_Problem-Solving-Divide & Conquer/49.3.7_prob-divide-conquer_Exercise/49.3.7_prob-divide-conquer_Working Copy/find-floor.js
@@ -1,5 +1,40 @@
-function findFloor() {
-  
-}
+// function findFloor(arr, value, low=0, high=arr.length-1) {
+//   if(high< low) return -1;
 
-module.exports = findFloor
+//   if(high === low && arr[low] <= value){
+//     return arr[low]
+//   }else if(high === low && arr[low] > value){
+//     return -1;
+//   }
+//   let mid = Math.floor((low + high)/2);
+//   console.log("stuff")
+//   if(arr[mid]<= value && value < arr[mid+1]){
+//     console.log("1")
+//     console.log(arr[mid+1])
+//     return arr[mid];
+//   }else if(arr[mid]<= value && mid== arr.length){
+//     console.log("2")
+//     return arr[mid]
+//   }else{
+//     return findFloor(arr, value, mid, high-1) 
+//   }
+// }
+
+function findFloor(arr, num, low = 0, high = arr.length - 1) {
+  if (low > high) return -1;
+  if (num >= arr[high]) return arr[high];
+
+  let mid = Math.floor((low + high) / 2)
+
+  if (arr[mid] === num) return arr[mid];
+
+  if (mid > 0 && arr[mid - 1] <= num && num < arr[mid]) {
+    return arr[mid - 1];
+  }
+
+  if (num < arr[mid]) {
+    return findFloor(arr, num, low, mid - 1);
+  }
+
+  return findFloor(arr, num, mid + 1, high)
+}
